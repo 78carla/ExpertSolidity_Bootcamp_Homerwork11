9d55e3f5791f867b603acfbacb4fad095548227d
78carla <78c.carla@gmail.com>
2023-05-11 11:46:54 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# HOMEWORK10
-
-Test the following statements which is cheaper and why ?
-Assume that the denominator can never be zero.
-
-1.  result = numerator / demoninator;
-2.  assembly {
-    result := div(numerator, demoninator)
-    }
+# HOMEWORK11
--- test/DogCoinGame.t.sol
+++ test/DogCoinGame.t.sol
@@ -2,29 +2,29 @@
 pragma solidity ^0.8.0; 
 
 import "../lib/forge-std/src/Test.sol";
-import "../src/isCheaper.sol";
+import "../src/DogCoinGame.sol";
 
 contract isCheaperTest is Test {
 
-    isCheaper public gc;
+    DogCoinGame public gc;
     address owner = address(0x1234);
 
         function setUp() public {
             
         vm.startPrank(owner);
-        gc = new isCheaper();
+        gc = new DogCoinGamer();
         //gas = new isCheaper();
         vm.stopPrank();
     }
 
 
-    function test_firstDiv() public {
+    // function test_firstDiv() public {
         
-        // Call the `firstDiv()` function with some input parameters
-        uint result = gc.firstDiv(64, 2);
-        // Check if the result is equal to the expected output
-        assertEq(result, 32, "Division operation failed");
-        }
+    //     // Call the `firstDiv()` function with some input parameters
+    //     uint result = gc.firstDiv(64, 2);
+    //     // Check if the result is equal to the expected output
+    //     assertEq(result, 32, "Division operation failed");
+    //     }
 
     //  function test_AssemblyDiv() public {
         
